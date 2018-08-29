f81913f8d6fc126122785dea333db67329ce09ad
Chingis Sandanov <chingis@wodby.com>
2018-08-29 16:57:54 +0700

    Clean up tests

--- tests/run.sh
+++ tests/run.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-#if [[ -n "${DEBUG}" ]]; then
+if [[ -n "${DEBUG}" ]]; then
     set -x
-#fi
+fi
 
 docker-compose build
 docker-compose up -d
 docker-compose exec postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
-docker-compose logs sidekiq
-docker-compose exec sidekiq ps aux
 docker-compose exec sidekiq ps aux | grep -q sidekiq
 docker-compose exec ruby tests.sh
 docker-compose down
