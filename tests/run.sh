3f275b55c77973448889125d2f34ea8ba4f08a37
Chingis Sandanov <chingis@wodby.com>
2018-08-29 16:09:40 +0700

    Add sidekiq service

--- tests/run.sh
+++ tests/run.sh
@@ -6,7 +6,9 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
+docker-compose build
 docker-compose up -d
 docker-compose exec postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
+docker-compose exec sidekiq ps aux | grep -q sidekiq
 docker-compose exec ruby tests.sh
 docker-compose down
