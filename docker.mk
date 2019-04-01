f242744a73d63e939a601310d5d0d617e2c25c00
Chingis Sandanov <chingis@wodby.com>
2019-04-01 17:00:03 +0700

    Add make help, make start commands. Allow pruning individual containers

--- docker.mk
+++ docker.mk
@@ -4,32 +4,55 @@ include .env
 
 default: up
 
+## help	:	Print commands help.
+help : docker.mk
+	@sed -n 's/^##//p' $<
+
+## up	:	Start up containers.
 up:
 	@echo "Starting up containers for for $(PROJECT_NAME)..."
 	docker-compose pull
 	docker-compose build
 	docker-compose up -d --remove-orphans
 
+## build	:	Build ruby image.
 build:
-	@echo "Building containers for for $(PROJECT_NAME)..."
+	@echo "Building ruby image for for $(PROJECT_NAME)..."
 	docker-compose build
 
+## start	:	Start containers without updating.
+start:
+	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
+	@docker-compose start
+
+## down	:	Stop containers.
 down: stop
 
+## stop	:	Stop containers.
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
 	@docker-compose stop
 
+## prune	:	Remove containers and their volumes.
+##		You can optionally pass an argument with the service name to prune single container
+##		prune mariadb	: Prune `mariadb` container and remove its volumes.
+##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
 prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
-	@docker-compose down -v
+	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
+## ps	:	List running containers.
 ps:
 	@docker ps --filter name='$(PROJECT_NAME)*'
 
+## shell	:	Access `ruby` container via shell.
 shell:
 	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_ruby' --format "{{ .ID }}") sh
 
+## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs ruby	: View `ruby` container logs.
+##		logs nginx ruby	: View `nginx` and `ruby` containers logs.
 logs:
 	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
