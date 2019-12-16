978cf4b6958758583e46e854738bbee37e1e43db
Chingis Sandanov <chingis@wodby.com>
2019-12-16 13:41:07 +0700

    Increase sidekiq startup time delay

--- tests/run.sh
+++ tests/run.sh
@@ -9,7 +9,7 @@ fi
 docker-compose build
 docker-compose up -d
 docker-compose exec postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
-sleep 5
+sleep 10
 docker-compose exec sidekiq ps aux | grep sidekiq
 docker-compose exec ruby tests.sh
 docker-compose down
