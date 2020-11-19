d6071e8db28ccdfd7e0fb4fc04fe5acb299eece8
Chingis Sandanov <chingis@wodby.com>
2020-11-20 02:36:22 +0700

    Migrate to gh actions

--- tests/run.sh
+++ tests/run.sh
@@ -8,8 +8,8 @@ fi
 
 docker-compose build
 docker-compose up -d
-docker-compose exec postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
+docker-compose exec -T postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
 sleep 10
-docker-compose exec sidekiq ps aux | grep sidekiq
-docker-compose exec ruby tests.sh
+docker-compose exec -T sidekiq ps aux | grep sidekiq
+docker-compose exec -T ruby tests.sh
 docker-compose down
