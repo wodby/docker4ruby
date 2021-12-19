e78e18d94b8d8dfe88e33679a89baccfcae50de7
Chingis Sandanov <chingis@wodby.com>
2021-12-19 13:37:25 +0800

    Clean up

--- tests/tests.sh
+++ tests/tests.sh
@@ -9,8 +9,4 @@ fi
 rake db:migrate
 rake db:seed
 
-if [[ "$(rails version)" =~ ^6. ]]; then
-  curl -s nginx | grep -q "Yay! You&rsquo;re on Rails!"
-else
-  curl -s nginx | grep -q "Rails version:"
-fi
+curl -s nginx | grep -q "Rails version:"
